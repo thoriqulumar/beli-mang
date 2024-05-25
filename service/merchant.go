22ef87c3a8e46138a827d0625ceef6641c247d49
thoriqulumar <bang.thor.tu@gmail.com>
2024-05-25 19:04:54 +0700

    feat: Post merchant item API

--- service/merchant.go
+++ service/merchant.go
@@ -11,6 +11,7 @@ import (
 type MerchantService interface {
 	CreateMerchant(request model.CreateMerchantRequest) (merchantId string, err error)
 	GetMerchant(ctx context.Context, params model.GetMerchantParams) (listMerchant []model.Merchant, meta model.MetaData, err error)
+	CreateMerchantItem(ctx context.Context, request model.CreateMerchantItemRequest, merchantId string) (itemId string, err error)
 }
 
 type merchantSvc struct {
@@ -50,4 +51,30 @@ func (s *merchantSvc) GetMerchant(ctx context.Context, params model.GetMerchantP
 	}
 
 	return listMerchant, meta, nil
+}
+
+
+func (s *merchantSvc) CreateMerchantItem(ctx context.Context, request model.CreateMerchantItemRequest, merchantId string) (itemId string, err error) {
+
+	_, err = s.repo.GetMerchantById(ctx, merchantId)
+	if err != nil {
+		return "", err
+	}
+
+	id := uuid.New()
+
+	merchantItem := model.MerchantItem{
+		ID:        id,
+		MerchantId: merchantId,
+		Name:      request.Name,
+		Category:  request.ProductCategory,
+		ImageURL:  request.ImageURL,
+	}
+
+	err = s.repo.CreateMerchantItem(merchantItem)
+	if err != nil {
+		return "", err
+	}
+
+	return id.String(), nil
 }
